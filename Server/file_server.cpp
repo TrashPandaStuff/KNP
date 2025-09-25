f5659e4820c6c28e2f36aed4b984d1cd0123351f
Nicolai Broholm Hansen <AU772678@uni.au.dk>
2025-09-25 11:05:03 +0200

    Add files via upload

---
+++ Server/file_server.cpp
@@ -0,0 +1,49 @@
+/* A simple server in the internet domain using TCP
+The port number is passed as an argument 
+Based on example: https://www.linuxhowtos.org/C_C++/socket.htm 
+
+Modified: Michael Alrøe
+Extended to support file server!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "iknlib.h"
+
+#define STRBUFSIZE 256
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+/**
+ * @brief Sends a file to a client socket
+ * @param clientSocket Socket stream to client
+ * @param fileName Name of file to be sent to client
+ * @param fileSize Size of file
+ */
+void sendFile(int clientSocket, const char* fileName, long fileSize)
+{
+	printf("Sending: %s, size: %li\n", fileName, fileSize);
+    
+	
+	
+}
+
+
+int main(int argc, char *argv[])
+{
+	printf("Starting server...\n");
+
+
+
+
+	return 0; 
+}
